ec8c1bf4f52e4b1a5720a2c6a23c1f4a3c6646ec
Boris Rybalkin <ribalkin@gmail.com>
2026-01-14 22:16:18 +0000

    patch

--- openwebui/build.sh
+++ openwebui/build.sh
@@ -7,3 +7,19 @@ mkdir -p ${BUILD_DIR}
 cp -r /bin ${BUILD_DIR}
 cp -r /usr ${BUILD_DIR}
 cp -r /lib ${BUILD_DIR}
+cp -r /app ${BUILD_DIR}
+
+apt update
+apt install -y patchelf
+
+SNAP=/snap/openwebui/current
+mkdir -p $SNAP
+ln -s ${BUILD_DIR} $SNAP/openwebui
+
+LD=$(echo $SNAP/isr/lib/*/ld-*.so*)
+LIBS=$(echo echo $SNAP/usr/lib/*linux*)
+echo $LD
+PYTHON=${BUILD_DIR}/usr/local/bin/python3
+patchelf --set-interpreter $LD $PYTHON
+patchelf --set-rpath $LIBS $PYTHON
+$PYTHON --version
--- openwebui/test.sh
+++ openwebui/test.sh
@@ -4,8 +4,4 @@ DIR=$( cd "$( dirname "$0" )" && pwd )
 cd ${DIR}
 
 BUILD_DIR=${DIR}/../build/snap/openwebui
-$BUILD_DIR/sbin/python --version
-$BUILD_DIR/sbin/python ${BUILD_DIR}/usr/local/bin/celery --version
-$BUILD_DIR/sbin/tesseract --list-langs | grep eng
-$BUILD_DIR/sbin/tesseract --list-langs
-$BUILD_DIR/sbin/convert --version
+$BUILD_DIR/usr/local/python --version
