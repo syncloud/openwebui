770c989397a9d9c547f4494e20338c556fe31167
Boris Rybalkin <ribalkin@gmail.com>
2026-01-15 07:36:14 +0000

    test snap

--- openwebui/test.sh
+++ openwebui/test.sh
@@ -4,4 +4,8 @@ DIR=$( cd "$( dirname "$0" )" && pwd )
 cd ${DIR}
 
 BUILD_DIR=${DIR}/../build/snap/openwebui
+SNAP=/snap/openwebui/current
+mkdir -p $SNAP
+ln -s ${BUILD_DIR} $SNAP/openwebui
+ls -la $BUILD_DIR/usr/local/bin/python3
 $BUILD_DIR/usr/local/bin/python3 --version
