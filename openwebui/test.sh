fe96d7f1d18ffbcbb90ab3828b39f3255f49064c
Boris Rybalkin <ribalkin@gmail.com>
2026-01-15 21:31:15 +0000

    ssl debug

--- openwebui/test.sh
+++ openwebui/test.sh
@@ -9,7 +9,9 @@ mkdir -p $SNAP
 ln -s ${BUILD_DIR} $SNAP/openwebui
 ls -la $BUILD_DIR/usr/local/bin/python3
 $BUILD_DIR/usr/local/bin/python3 --version
-export PYTHONPATH="$BUILD_DIR/usr/local/lib/python3.11/site-packages"
+export PYTHONPATH="$BUILD_DIR/usr/local/lib/python3.11/site-packages:$BUILD_DIR/usr/local/lib/python3.11/lib-dynload"
 export LD_LIBRARY_PATH=$(echo $BUILD_DIR/usr/lib/*linux*/)
+strings $BUILD_DIR/usr/local/lib/python3.11/lib-dynload/_ssl.cpython-311-*-linux-gnu.so  | grep PyMod
+
 $BUILD_DIR/usr/local/bin/python3 -c "import ssl"
 $BUILD_DIR/usr/local/bin/python3 -m uvicorn --version
