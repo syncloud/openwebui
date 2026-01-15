a4c0a8634428d53828dd620e5cca5ee21c570094
Boris Rybalkin <ribalkin@gmail.com>
2026-01-15 20:56:21 +0000

    test

---
+++ bin/service.webui.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+DIR=$( cd "$( dirname "$0" )" && cd .. && pwd )
+
+export WEBUI_SECRET_KEY=$(cat $SNAP_DATA/webui_secret_key)
+exec $DIR/openwebui/usr/local/bin//python3 \
+    -m uvicorn open_webui.main:app \
+    --uds $SNAP_COMMON\web.socket
+    --forwarded-allow-ips '*' \
+    --workers 1
+   
