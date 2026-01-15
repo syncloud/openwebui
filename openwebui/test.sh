189d6c4b9fc45431b73d00322bee689aa45cc397
Boris Rybalkin <ribalkin@gmail.com>
2026-01-15 21:36:13 +0000

    binutils

--- openwebui/test.sh
+++ openwebui/test.sh
@@ -11,6 +11,10 @@ ls -la $BUILD_DIR/usr/local/bin/python3
 $BUILD_DIR/usr/local/bin/python3 --version
 export PYTHONPATH="$BUILD_DIR/usr/local/lib/python3.11/site-packages:$BUILD_DIR/usr/local/lib/python3.11/lib-dynload"
 export LD_LIBRARY_PATH=$(echo $BUILD_DIR/usr/lib/*linux*/)
+
+apt update
+apt install binutils
+
 strings $BUILD_DIR/usr/local/lib/python3.11/lib-dynload/_ssl.cpython-311-*-linux-gnu.so  | grep PyMod
 
 $BUILD_DIR/usr/local/bin/python3 -c "import ssl"
