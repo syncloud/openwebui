47eba07cd5581c4575c2799a6a7a3a5360ce4257
Boris Rybalkin <ribalkin@gmail.com>
2026-01-17 11:18:12 +0000

    clean test

--- openwebui/build.sh
+++ openwebui/build.sh
@@ -19,24 +19,26 @@ SNAP=/snap/openwebui/current
 mkdir -p $SNAP
 ln -s ${BUILD_DIR} $SNAP/openwebui
 
+#python --version
+find / -name "*.pyc" -delete
+find / -name "__pycache__" -exec rm -rf {} +
+strace python -c "import ssl; print(ssl.OPENSSL_VERSION)" 2>&1 | grep -v "No such file"
+#python -m uvicorn --version
+
+#strings /usr/local/lib/python3.11/lib-dynload/_ssl.cpython-311-*-linux-gnu.so  | grep PyMod
+
+#$BUILD_DIR/bin/python --version
+find / -name "*.pyc" -delete
+find / -name "__pycache__" -exec rm -rf {} +
+strace $BUILD_DIR/bin/python -c "import ssl; print(ssl.OPENSSL_VERSION)" 2>&1 | grep -v "No such file"
+#$BUILD_DIR/bin/python -m uvicorn --version
+
+PYTHON=${BUILD_DIR}/usr/local/bin/python3
+ldd $PYTHON
 LD=$(echo $SNAP/openwebui/usr/lib/*/ld-*.so*)
 LIBS=$(echo $SNAP/openwebui/usr/lib/*linux*/)
 echo $LD
 echo $LIBS
-
-python --version
-strace python -c "import ssl; print(ssl.OPENSSL_VERSION)" 2>&1 | grep -v "No such file"
-python -m uvicorn --version
-
-#strings /usr/local/lib/python3.11/lib-dynload/_ssl.cpython-311-*-linux-gnu.so  | grep PyMod
-
-$BUILD_DIR/bin/python --version
-strace $BUILD_DIR/bin/python -c "import ssl; print(ssl.OPENSSL_VERSION)" 2>&1 | grep -v "No such file"
-$BUILD_DIR/bin/python -m uvicorn --version
-
-PYTHON=${BUILD_DIR}/usr/local/bin/python3
-ldd $PYTHON
-
 patchelf --set-interpreter $LD $PYTHON
 patchelf --set-rpath $LIBS $PYTHON
 $PYTHON --version
