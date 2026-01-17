5494577047f8f088f91bc72d1cf9617d7d2b5290
Boris Rybalkin <ribalkin@gmail.com>
2026-01-17 11:28:55 +0000

    remove pth

--- openwebui/build.sh
+++ openwebui/build.sh
@@ -4,6 +4,9 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 BUILD_DIR=${DIR}/../build/snap/openwebui
 mkdir -p ${BUILD_DIR}
+
+rm -rf /usr/local/lib/python3.11/site-packages/*.pth
+
 cp -r /bin ${BUILD_DIR}
 cp -r /usr ${BUILD_DIR}
 cp -r /lib ${BUILD_DIR}
