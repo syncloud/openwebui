fc4fb2265d543436e17d70c6d7831f22ee721564
Boris Rybalkin <ribalkin@gmail.com>
2026-01-17 15:55:02 +0000

    test

--- openwebui/build.sh
+++ openwebui/build.sh
@@ -7,34 +7,15 @@ mkdir -p ${BUILD_DIR}
 
 rm -rf /usr/local/lib/python3.11/site-packages/*.pth
 
-#cp -r /bin ${BUILD_DIR}
 cp -r /usr ${BUILD_DIR}
-#cp -r /lib ${BUILD_DIR}
 cp -r /app ${BUILD_DIR}
-#cp -r $DIR/bin/* ${BUILD_DIR}/bin/
-#find ${BUILD_DIR} -name "*.pyc" -delete
-#find ${BUILD_DIR} -name "__pycache__" -exec rm -rf {} +
 
 apt update
-apt install -y patchelf strace
+apt install -y patchelf
 
 SNAP=/snap/openwebui/current
 mkdir -p $SNAP
 ln -s ${BUILD_DIR} $SNAP/openwebui
-
-#python --version
-#find / -name "*.pyc" -delete
-#find / -name "__pycache__" -exec rm -rf {} +
-python -c "import ssl; print(ssl.OPENSSL_VERSION)" 2>&1 | grep -v "No such file"
-python -m uvicorn --version
-
-#strings /usr/local/lib/python3.11/lib-dynload/_ssl.cpython-311-*-linux-gnu.so  | grep PyMod
-
-#$BUILD_DIR/bin/python --version
-#find / -name "*.pyc" -delete
-#find / -name "__pycache__" -exec rm -rf {} +
-#strace $BUILD_DIR/bin/python -c "import ssl; print(ssl.OPENSSL_VERSION)" 2>&1 | grep -v "No such file"
-#$BUILD_DIR/bin/python -m uvicorn --version
 
 PYTHON=${BUILD_DIR}/usr/local/bin/python3
 ldd $PYTHON
