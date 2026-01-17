e4bc8cc7a4e287410c81a7781e612b0ad7bb3f78
Boris Rybalkin <ribalkin@gmail.com>
2026-01-17 15:46:06 +0000

    lib order

--- openwebui/build.sh
+++ openwebui/build.sh
@@ -7,13 +7,13 @@ mkdir -p ${BUILD_DIR}
 
 rm -rf /usr/local/lib/python3.11/site-packages/*.pth
 
-cp -r /bin ${BUILD_DIR}
+#cp -r /bin ${BUILD_DIR}
 cp -r /usr ${BUILD_DIR}
-cp -r /lib ${BUILD_DIR}
+#cp -r /lib ${BUILD_DIR}
 cp -r /app ${BUILD_DIR}
 cp -r $DIR/bin/* ${BUILD_DIR}/bin/
-find ${BUILD_DIR} -name "*.pyc" -delete
-find ${BUILD_DIR} -name "__pycache__" -exec rm -rf {} +
+#find ${BUILD_DIR} -name "*.pyc" -delete
+#find ${BUILD_DIR} -name "__pycache__" -exec rm -rf {} +
 
 apt update
 apt install -y patchelf strace
@@ -23,27 +23,29 @@ mkdir -p $SNAP
 ln -s ${BUILD_DIR} $SNAP/openwebui
 
 #python --version
-find / -name "*.pyc" -delete
-find / -name "__pycache__" -exec rm -rf {} +
-strace python -c "import ssl; print(ssl.OPENSSL_VERSION)" 2>&1 | grep -v "No such file"
-#python -m uvicorn --version
+#find / -name "*.pyc" -delete
+#find / -name "__pycache__" -exec rm -rf {} +
+python -c "import ssl; print(ssl.OPENSSL_VERSION)" 2>&1 | grep -v "No such file"
+python -m uvicorn --version
 
 #strings /usr/local/lib/python3.11/lib-dynload/_ssl.cpython-311-*-linux-gnu.so  | grep PyMod
 
 #$BUILD_DIR/bin/python --version
-find / -name "*.pyc" -delete
-find / -name "__pycache__" -exec rm -rf {} +
+#find / -name "*.pyc" -delete
+#find / -name "__pycache__" -exec rm -rf {} +
 #strace $BUILD_DIR/bin/python -c "import ssl; print(ssl.OPENSSL_VERSION)" 2>&1 | grep -v "No such file"
 #$BUILD_DIR/bin/python -m uvicorn --version
 
 PYTHON=${BUILD_DIR}/usr/local/bin/python3
 ldd $PYTHON
 LD=$(echo $SNAP/openwebui/usr/lib/*/ld-*.so*)
-LIBS=$(echo $SNAP/openwebui/usr/lib/*linux*/)
 LIBS=$LIBS:$SNAP/openwebui/usr/local/lib
+LIBS=$LIBS:$(echo $SNAP/openwebui/usr/lib/*linux*/)
+
 echo $LD
 echo $LIBS
 patchelf --set-interpreter $LD $PYTHON
 patchelf --set-rpath $LIBS $PYTHON
-#$PYTHON --version
-$PYTHON -c "import ssl; print(ssl.OPENSSL_VERSION)" 2>&1 | grep -v "No such file"
+$PYTHON --version
+$PYTHON -c "import ssl; print(ssl.OPENSSL_VERSION)"
+$PYTHON -m uvicorn --version
