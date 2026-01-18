30ec39b349931a9120c960abe9b11a94052b0312
Boris Rybalkin <ribalkin@gmail.com>
2026-01-18 09:24:22 +0000

    no buuld

--- openwebui/build.sh
+++ openwebui/build.sh
@@ -5,9 +5,9 @@ cd ${DIR}
 BUILD_DIR=${DIR}/../build/snap/openwebui
 mkdir -p ${BUILD_DIR}
 
-rm -rf /usr/local/lib/python3.11/site-packages/*.pth
-
+#rm -rf /usr/local/lib/python3.11/site-packages/*.pth
 cp -r /usr ${BUILD_DIR}
+rm -rf /app/build
 cp -r /app ${BUILD_DIR}
 
 apt update
