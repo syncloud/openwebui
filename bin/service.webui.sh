8bb39de2b4356ba54d24036439d58d2e8d4433fc
Boris Rybalkin <ribalkin@gmail.com>
2026-01-18 10:42:05 +0000

    web socket

--- bin/service.webui.sh
+++ bin/service.webui.sh
@@ -6,6 +6,6 @@ cd $DIR/openwebui/app/backend
 source $SNAP_DATA/config/.env
 exec $DIR/openwebui/usr/local/bin/python3 \
     -m uvicorn open_webui.main:app \
-    --uds $SNAP_COMMON\web.socket
+    --uds $SNAP_COMMON/web.socket \
     --forwarded-allow-ips '*' \
     --workers 1
