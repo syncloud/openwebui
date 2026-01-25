40a65cac64ae6d8b79e740e23aecefafcbb7e1c1
Boris Rybalkin <ribalkin@gmail.com>
2026-01-25 15:03:20 +0000

    certs

--- bin/service.webui.sh
+++ bin/service.webui.sh
@@ -2,6 +2,7 @@
 
 DIR=$( cd "$( dirname "$0" )" && cd .. && pwd )
 
+export REQUESTS_CA_BUNDLE=/var/snap/platform/current/syncloud.ca.crt
 export PATH=$DIR/openwebui/bin:$PATH
 cd $DIR/openwebui/app/backend
 source $SNAP_DATA/config/openwebui.env
