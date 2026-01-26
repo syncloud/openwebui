2c4f36d9dcd3fa867123031f1491612ed2ef881d
Boris Rybalkin <ribalkin@gmail.com>
2026-01-26 08:29:02 +0000

    try ssl no verify

--- bin/service.webui.sh
+++ bin/service.webui.sh
@@ -2,11 +2,10 @@
 
 DIR=$( cd "$( dirname "$0" )" && cd .. && pwd )
 
-if [[ -f /var/snap/platform/current/CI_TEST ]]; then
-  export HTTPX_VERIFY=false
-fi
-export SSL_CERT_FILE=/var/snap/platform/current/syncloud.ca.crt
-export REQUESTS_CA_BUNDLE=/var/snap/platform/current/syncloud.ca.crt
+export OPENSSL_VERIFY=0
+export HTTPX_VERIFY=false
+#export SSL_CERT_FILE=/var/snap/platform/current/syncloud.ca.crt
+#export REQUESTS_CA_BUNDLE=/var/snap/platform/current/syncloud.ca.crt
 export PATH=$DIR/openwebui/bin:$PATH
 cd $DIR/openwebui/app/backend
 source $SNAP_DATA/config/openwebui.env
