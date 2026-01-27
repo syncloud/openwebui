7bc449c375122f52d6611f6496f18897ca10c545
Boris Rybalkin <ribalkin@gmail.com>
2026-01-27 19:05:41 +0000

    curl domain

--- test/test.py
+++ test/test.py
@@ -62,7 +62,7 @@ def test_activate_device(device):
     assert response.status_code == 200, response.text
 
 
-def test_ca_cert(device, app_domain):
+def test_ca_cert(device, app_domain, domain):
     device.run_ssh('cp /var/snap/platform/current/syncloud.ca.crt /usr/local/share/ca-certificates')
     device.run_ssh('update-ca-certificates 2>&1 > {0}/update-ca-certificates.log'.format(TMP_DIR))
     device.run_ssh('curl -v https://{0} 2>&1 > {1}/curl.app.log'.format(app_domain, TMP_DIR))
